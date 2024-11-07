df428b25769a96533236f8285534c822e79bd5ab
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-11-07 22:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
-  // your code here
+  // your code heredfs
+s =s.trim();
+	if(s===" "){
+		return " ";
+	}
+	const spaceindex =s.indexof('');
+	return spaceindex === -1 ?s:s.substring(0,spaceindex);
 }
+
+cosnt s ="see and stop ";
+console.log(firstWord(s));
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
