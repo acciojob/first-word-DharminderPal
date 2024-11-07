5440f739b8b15d2ee4478e49c28586a01b3a652c
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-11-07 22:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-function firstWord(s) {
-  // your code heredfs
-s =s.trim();
-	if(s===" "){
-		return " ";
-	}
-	const spaceindex =s.indexof('');
-	return spaceindex === -1 ?s:s.substring(0,spaceindex);
-}
+// function firstWord(s) {
+//   // your code heredfs
+// s =s.trim();
+// 	if(s===" "){
+// 		return " ";
+// 	}
+// 	const spaceindex =s.indexof('');
+// 	return spaceindex === -1 ?s:s.substring(0,spaceindex);
+// }
 
-cosnt s ="see and stop ";
-console.log(firstWord(s));
+// const s ="see and stop ";
+// console.log(firstWord(s));
 
 // Do not change the code below
-
 // const s = prompt("Enter String:");
 // alert(firstWord(s));
+function firstWord(s) {
+  s = s.trim();
+  if(s === " "){
+    return " ";
+  }
+  const spaceIndex = s.indexOf(' ');
+  return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
+}
+
+const s = "see and stop ";
+console.log(firstWord(s));
